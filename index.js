1f03f072d9bb7d9e94f3e83487a4b2cf24a2e7c6
sonny faristrianto <sonnyf4r15@gmail.com>
2024-02-05 23:12:07 +0700

    tambah index

---
+++ index.js
@@ -0,0 +1,10 @@
+function panggilSort(){
+var data =[31,33,16,40,9,7,11,2,44,1,66]
+console.log('sebelumnya : ',data);
+data = data.sort();
+console.log('ascending : ', data);
+data2 = data.reverse();
+console.log('descending : ', data2);
+}
+
+panggilSort();
